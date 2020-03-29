963b03e759ee1b7e9a587653ebd027256f190ab1
Michelle Salazar <michellesalazar010@gmail.com>
2020-03-28 21:20:51 -0700

    loops

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'use strict'; 
 
-//Name question
+//1.Name question
 var question1 = prompt('What is your name?');
 console.log(question1);
-
-//Travel question
+/*
+//2.Travel question
 var question2 = prompt(question1 + ' Do you like to travel');
 
 if(question2.toLowerCase() === 'yes'){
@@ -20,7 +20,7 @@ else {
     alert(question1 + ' I know its difficult to get to know each other this way, but i know were going to be friends for a long time');
 }
 
-//SCIFI
+//3.SCIFI
 var question3 = prompt('Do you belive in the unexplained?');
 if(question3.toLocaleLowerCase() === 'yes'){
   alert(question1+ ' I love science and the mystery of what we dont know...\(insert xfiles theme song here)');
@@ -32,28 +32,67 @@ else if(question3.toLocaleLowerCase() === 'no'){
 else{ 
     alert(question1 + ' These are the things that are important to me, to get to know me it will be easier if we could interact a bit more.');
 }
-//Tech
-var question4 = prompt('I have grown up with technology my whole life '+question1+' do you have much experience with computers and their programming?');
+//4.movies
+var question4 = prompt('Have you seen Empire Records?');
+if(question4.toLocaleLowerCase() === 'yes'){
+  alert(question1+ ' This is one of my background movies; good music, good actors and great movie quotes. The charcaters are so relatable');
+} 
+else if(question4.toLocaleLowerCase() === 'no'){
+  alert(question1+ ' You\'d have to be a 90\'s kid to remember it');
+} 
+// no answer - bypass yes/no answers
+else{ 
+    alert(question1 + ' I recommend this movie if you like to watch underrated movies.');
+}
 
-if(question4.toLowerCase() === 'yes'){
+//5.Tech
+var question5 = prompt('I have grown up with technology my whole life '+question1+' do you have much experience with computers and their programming?');
+
+if(question5.toLowerCase() === 'yes'){
   alert(question1+ ' My dad built and programmed computers, growing up we had tons of equipment all over my house. I learned how to use computers before they were even a common thing to have in households.');
 } 
-else if(question4.toLowerCase() === 'no'){
+else if(question5.toLowerCase() === 'no'){
   alert(question1+ ' Computers can be tough to learn if you have no desire to learn the intricacies of computing.');
 } 
 // no answer - bypass yes/no answers
 else{ 
     alert(question1+ ' I see this is not a topic your\'re into. Lets learn more about each other');
   }
-//Family
-var question5 = prompt('Do you have a big family?');
-if(question5.toLowerCase() === 'yes'){
+//6.Family
+var question6 = prompt('Do you have a big family?');
+if(question6.toLowerCase() === 'yes'){
   alert(question1+ ' Thats amazing! I have a small family, but I love them. Family get togethers are my favorite thing to do.');
 } 
-else if (question5.toLowerCase() === 'no'){
+else if (question6.toLowerCase() === 'no'){
   alert(' Me either, I guess that means theres more turkey for seconds during the holidays. amma right?? ' +question1+ ' Holidays can be so stressfull when you have to attend more than one dinner or gathering.');
 } 
 // no answer - bypass yes/no answers
 else{ 
   alert(question1+ ' I guess this is the end of our connection. Im sorry we didnt have more things in common but I enjoyed our interaction.');
 }
+*/
+//7.years in college
+var years = 10;
+
+
+for (var i = 0; i < 5; i++);
+{document.write(i); 
+  
+  var question7 = prompt(question1 + 'Can you guess how many years it too for me to obtain my Associates degree?');
+console.log(question7)
+while(i === years ){
+if(question7 === 10)
+{
+  alert(question1+ ' Yes it took me ' + question7+ 'years to complete my associates degree. It was mostly being distracted that took me so long');
+} 
+else if (question7 < 10)
+{ 
+  alert(question1 + ' Your guess is too low,  guess again!');
+} 
+
+else if (question7 > 10)
+{ 
+  alert(question1 + ' Your guess is too high,  guess again!');
+} 
+counter++
+}}
